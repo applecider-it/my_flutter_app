5d43f7074281433399d45758bd3a972148221b20
owner <owner@localhost>
2026-04-10 12:52:15 +0900

    カメラ

--- src/lib/screens/development_page.dart
+++ src/lib/screens/development_page.dart
@@ -1,6 +1,7 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
 
 import 'home_page.dart';
 
@@ -26,6 +27,11 @@ class _DevelopmentPageState extends State<DevelopmentPage> {
   TextEditingController passwordController = TextEditingController(
     text: "1234",
   );
+
+  bool isCameraInit = false;
+  late CameraController controller;
+  late List<CameraDescription> cameras;
+  XFile? imageFile;
 
   final authCtrl = AuthCtrl();
 
@@ -101,6 +107,43 @@ class _DevelopmentPageState extends State<DevelopmentPage> {
     );
   }
 
+  Future<void> execCamera() async {
+    print("execCamera");
+
+    if (!isCameraInit) {
+      WidgetsFlutterBinding.ensureInitialized();
+      cameras = await availableCameras();
+
+      controller = CameraController(
+        cameras[0], // 背面カメラ
+        ResolutionPreset.medium,
+      );
+
+      await controller.initialize();
+
+      isCameraInit = true;
+    }
+
+    final image = await controller.takePicture();
+    print("takePicture");
+    print(image.path); // 保存パス
+
+    setState(() {
+      imageFile = image;
+    });
+  }
+
+  // 終了処理
+  @override
+  void dispose() {
+    if (isCameraInit) {
+      controller.dispose();
+
+      isCameraInit = false;
+    }
+    super.dispose();
+  }
+
   // UI構築
 
   @override
@@ -125,7 +168,7 @@ class _DevelopmentPageState extends State<DevelopmentPage> {
               decoration: InputDecoration(labelText: "Password"),
             ),
 
-            const SizedBox(height: 20), // 間隔を空ける
+            const SizedBox(height: 10), // 間隔を空ける
             // ログインボタン
             ElevatedButton(
               onPressed: () async {
@@ -134,7 +177,7 @@ class _DevelopmentPageState extends State<DevelopmentPage> {
               child: Text("ログイン"),
             ),
 
-            const SizedBox(height: 100), // 間隔を空ける
+            const SizedBox(height: 10), // 間隔を空ける
             // ログアウトボタン
             ElevatedButton(
               onPressed: () async {
@@ -142,6 +185,28 @@ class _DevelopmentPageState extends State<DevelopmentPage> {
               },
               child: Text("ログアウト"),
             ),
+
+            const SizedBox(height: 10), // 間隔を空ける
+            // カメラボタン
+            ElevatedButton(
+              onPressed: () async {
+                await execCamera();
+              },
+              child: Text("カメラ起動+撮影"),
+            ),
+
+            if (isCameraInit)
+              Expanded(
+                child: controller.value.isInitialized
+                    ? CameraPreview(controller)
+                    : Container(),
+              ),
+
+            if (imageFile != null)
+              Image.file(
+                File(imageFile!.path),
+                height: 200,
+              ),
           ],
         ),
       ),
