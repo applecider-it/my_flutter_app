a14ece47ebc79a3e164894c9e3c0a466b74d0feb
owner <owner@localhost>
2026-04-16 09:41:42 +0900

    apiのバージョン

--- node/app/config/routes.ts
+++ node/app/config/routes.ts
@@ -1,6 +1,6 @@
 import { Express } from "express";
 
-import authController from "@/controllers/auth.controller.js";
+import authController from "@/controllers/v1/auth.controller.js";
 
 export const setRoutes = (app: Express) => {
   app.post("/v1/login", authController.login);
